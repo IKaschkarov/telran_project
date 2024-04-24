861dfd2221ac5104a930969f5728d2c80e8e8502
IKaschkarov <142896713+IKaschkarov@users.noreply.github.com>
2024-04-24 21:18:12 +0200

    Added styles for Footer

--- project_garden/src/components/Footer/Footer.jsx
+++ project_garden/src/components/Footer/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import styles from './Footer.module.css'
 
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+        Footer
+
+    </div>
   )
 }
